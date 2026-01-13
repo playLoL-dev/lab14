9a19b7fdc4a900a34f69e0e99644f571685cc4b0
playLoL <playza.1987@gmail.com>
2026-01-13 18:06:18 +0700

    lab14_2.cpp completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,43 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+//Do not modify source code above this line
+
+void showMatrix(const bool B[][N]){
+	for( int i=0 ; i<N ; i++){
+		for( int j=0 ; j<N ; j++){
+			cout << B[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void inputMatrix(double A[][N]){
+	for( int i=0 ; i<N ; i++){
+		cout << "Row " << i+1 << ": ";
+		for( int j=0 ; j<N ; j++){
+			cin >> A[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double Matrix[][N], bool LocalMax[][N]){
+	for(int i = 0;i < N;i++){
+		for(int j = 0;j < N;j++){
+			LocalMax[i][j] = false;
+		}
+	}	
+
+	for(int i = 1;i < N - 1;i++){
+		for(int j = 1;j < N-1;j++){
+			if(
+			Matrix[i][j] >= Matrix[i][j+1] and 
+			Matrix[i][j] >= Matrix[i][j-1] and
+			Matrix[i][j] >= Matrix[i+1][j] and
+			Matrix[i][j] >= Matrix[i-1][j]
+			){
+				LocalMax[i][j] = true;
+			}
+		}
+	}
+}
